da478993729bf16616ab2efe98a661818d4919ad
Junior Corredor <rsi.dev24@uis.edu.co>
2023-12-12 11:54:28 -0500

    Components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet  name="Bruce" superHeroName="Batman">
+        this is children props
+      </Greet>
+      <Greet name="Clark" superHeroName="Superman"/>
+      <Greet name="Diana" superHeroName="Wonder Woman"/>
+
+      <Welcome name="Bruce" superHeroName="Batman"/>
+      {/*<Hello /> */}
     </div>
   );
 }
