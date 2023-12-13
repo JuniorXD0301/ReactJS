d0fae4f11553cdc3e52886bc752ba00b328f4b1a
Junior Corredor <rsi.dev24@uis.edu.co>
2023-12-13 09:53:28 -0500

    Handlers

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,11 +5,19 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 
 function App() {
   return (
     <div className="App">
+      <ParentComponent />
+      {/* <EventBind /> */}
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
       {/*<Counter />*/}
       {/* <Greet  name="Bruce" superHeroName="Batman">
         this is children props
